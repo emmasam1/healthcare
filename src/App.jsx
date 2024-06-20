f8fbec265986bd124d6a4390b83a7131022c7f67
Emmanuel <findm4@gmail.com>
2024-06-20 12:03:12 +0100

    fixed some issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Home from "./pages/Home";
 import Layout from "./layout/Layout";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import ScrollToTop from "./components/top/ScrollToTop";
 
 function App() {
   return (
     <>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="" element={<Home />} />
